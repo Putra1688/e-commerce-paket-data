9973af361f8f8a0ec6b1cfc22e5bf6266d82e711
ranggadsaputra <ranggadsaputra11@gmail.com>
2025-10-21 22:45:11 +0700

    add json server

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://my-json-server.typicode.com/Putra1688/e-commerce-paket-data';
 
 const useApi = (endpoint) => {
   const [data, setData] = useState(null);
